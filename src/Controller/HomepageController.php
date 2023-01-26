9be13fdd3089947f6ad0cf36f6f54060b2407d92
LOliviaL <liuhaoyue1024@gmail.com>
2023-01-26 15:56:20 +0100

    olivia

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/homepage', name: 'app_homepage', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+}
---
+++ src/Controller/ProtectWildlifeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProtectWildlifeController extends AbstractController
+{
+    #[Route('/protect/wildlife', name: 'app_protect_wildlife', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('protect_wildlife/index.html.twig', [
+            'controller_name' => 'ProtectWildlifeController',
+        ]);
+    }
+}
