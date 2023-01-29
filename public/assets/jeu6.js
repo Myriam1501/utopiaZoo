5331152fdb6e2173b64d2f4e2e572cdc062e7a6f
Meryam <meryamghulam@gmail.com>
2023-01-29 10:11:45 +0100

    game css

--- public/assets/jeu6.js
+++ public/assets/jeu6.js
@@ -44,16 +44,17 @@ function verif(){
     let img7=document.getElementById("img7").getAttribute('src');
     let img8=document.getElementById("img8").getAttribute('src');
     let img9=document.getElementById("img9").getAttribute('src');
-    if(img1==="http://brouillon.local/taquin/img1.jpg"){
-        if(img2==="http://brouillon.local/taquin/img9.jpg"){
-            if(img3==="http://brouillon.local/taquin/img2.jpg"){
-                if(img4==="http://brouillon.local/taquin/img3.jpg"){
-                    if(img5==="http://brouillon.local/taquin/img4.jpg"){
-                        if(img6==="http://brouillon.local/taquin/img5.jpg"){
-                            if(img7==="http://brouillon.local/taquin/img6.jpg"){
-                                if(img8==="http://brouillon.local/taquin/img7.jpg"){
+    alert(img1);
+    if(img1==="http://utopiazoo.local/taquin/img1.jpg"){
+        if(img2==="http://utopiazoo.local/taquin/img9.jpg"){
+            if(img3==="http://utopiazoo.local/taquin/img2.jpg"){
+                if(img4==="http://utopiazoo.local/taquin/img3.jpg"){
+                    if(img5==="http://utopiazoo.local/taquin/img4.jpg"){
+                        if(img6==="http://utopiazoo.local/taquin/img5.jpg"){
+                            if(img7==="http://utopiazoo.local/taquin/img6.jpg"){
+                                if(img8==="http://utopiazoo.local/taquin/img7.jpg"){
                                     if(img9===""){
-                                        $("#img9").attr('src',"http://brouillon.local/taquin/img8.jpg");
+                                        $("#img9").attr('src',"http://utopiazoo.local/taquin/img8.jpg");
                                         document.body.style.backgroundColor="green";
                                         let ding = new Audio('https://universal-soundbank.com/sounds/9669.mp3');
                                         ding.play();
