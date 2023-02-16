0317081820ad071e1589852925bf4d9e76fb2b5f
Myriam1501 <myriamo93500@gmail.com>
2023-02-16 10:55:50 +0100

    migrate programme

---
+++ src/FormHandler/UserFormHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\FormHandler;
+
+use App\Entity\Figure;
+use App\Entity\Programme;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class UserFormHandler
+{
+    public function __construct(
+        public EntityManagerInterface $entityManager
+    ) {}
+
+    public function handleForm(Programme $programme): void
+    {
+        $user = new User();
+        $user->setFirstName('Yassin');
+
+        $this->entityManager->persist($user);
+
+        $programme->setUser($user);
+
+        $this->entityManager->persist($programme);
+        $this->entityManager->flush();
+    }
+}
