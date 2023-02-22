524df5562eb98991bc5f1ebb774cae7f617fd7c3
Meryam <meryamghulam@gmail.com>
2023-02-22 07:53:57 +0100

    reservation service

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Program;
+use App\Entity\Reservation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReservationController extends AbstractController
 {
     #[Route('/reservation', name: 'app_reservation', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request,ManagerRegistry $doctrine): Response
     {
+
+        $entityManager = $doctrine->getManager();
+
+        $reserv = new Reservation();
+        $reserv->setQteNormal(0);
+        $reserv->setQteReduce(0);
+
+        $program = new Program();
+        $program->setDescriptionProgram("brrgsrbb dfbd");
+        $program->setPrice(1234);
+        $program->setAgeMin(2);
+        $program->setTitle('utopia-fantastic');
+
+
+
+        $reserv->addProgram($program);
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($reserv);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+
         return $this->render('reservation/index.html.twig', [
             'controller_name' => 'ReservationController',
+            'reservation' => $reserv,
         ]);
     }
 }
--- src/Services/CartServices.php
+++ src/Services/CartServices.php
@@ -5,6 +5,7 @@ use App\Entity\Account;
 use App\Entity\Reservation;
 use App\Repository\ProgramRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\HttpFoundation\Request;
 use  Symfony\Component\HttpFoundation\Session\SessionInterface;
 use App\Entity\Program;
@@ -27,45 +28,67 @@ class CartServices
 
     public function incrementeReservation()
     {
-        // recup la donnéee avec le $id
-        // INCREMENTER QTN
+        $newQtn=$this->reservation->getQteNormal();
+        $newQtn++;
+        $this->reservation->setQteNormal($newQtn);
 
     }
 
     public function supprimeReserva()
     {
+        $newQtn=$this->reservation->getQteNormal();
+        if ($newQtn>0){
+            $newQtn--;
+            $this->reservation->setQteNormal($newQtn);
+        }
+
         //recuperer donnée avec id et decrementer par rapport au nbrTodelte
 
     }
 
     public function supprimeToutReserva()
     {
+        $this->reservation->setQteNormal(0);
         //qtn=0
+
+    }
+
+    public function getTotalPriceByProgram() : Integer
+    {
+        $prixProgram=$this->reservation->getPrograms()->first()->getPrice();
+        $qtn=$this->reservation->getQteNormal();
+        $coutTotal=$qtn*$prixProgram;
+        return $coutTotal;
+    }
+
+    public function recupererTotalMontant() : Integer
+    {
+        return $this->getTotalPriceByProgram();
+        //recup qtn et prix et calcul
+    }
+
+    public function recupTotalApresTVA() : Integer
+    {
+        //appel montant totzl et *appliquer tva
+        $calculPourcent=$this->recupererTotalMontant()*(20/100);
+        $calculAvecTva=$this->recupererTotalMontant()+$calculPourcent;
+        return $calculAvecTva;
+    }
+
+
+
+
+    public function addAccount($idaccount)
+    {
+
+        //insertion de donnée avec account id=idaccount ET QTN=1
 
     }
 
     public function validerReservation()
     {
         //generer id pour le payment et passer au paiement
+        //CREE OBJET PAIMENT
     }
-
-    public function recupTotalApresTVA()
-    {
-        //appel montant totzl et *appliquer tva
-    }
-
-
-    public function recupererTotalMontant()
-    {
-        //recup qtn et prix et calcul
-    }
-
-
-    public function creeReservation($idaccount)
-    {
-        //insertion de donnée avec account id=idaccount ET QTN=1
-
-    }
-
 }
 ?>
