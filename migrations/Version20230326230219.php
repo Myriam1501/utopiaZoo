b075f517ae4b32dcac3bd0c0ff38059f96d28f75
Myriam1501 <myriamo93500@gmail.com>
2023-03-27 01:06:23 +0200

    delete branch and essay

--- migrations/Version20230326230219.php
+++ migrations/Version20230326230219.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230324230200 extends AbstractMigration
+final class Version20230326230219 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20230324230200 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE reservation ADD price DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE reservation ADD user_id INT NOT NULL');
         $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_42C84955A76ED395 ON reservation (user_id)');
     }
@@ -30,6 +30,6 @@ final class Version20230324230200 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955A76ED395');
         $this->addSql('DROP INDEX IDX_42C84955A76ED395 ON reservation');
-        $this->addSql('ALTER TABLE reservation DROP price');
+        $this->addSql('ALTER TABLE reservation DROP user_id');
     }
 }
